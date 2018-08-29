2afff0d5a3494b2f55aaf48641cc199514fe4c38
denpamusic <denpa@netfleet.space>
2018-08-30 02:22:11 +0300

    Fixed indentation.

--- neva/__init__.py
+++ neva/__init__.py
@@ -1,12 +1,12 @@
 from neva.meter import Meter
 
 def connect(url, **kwargs):
-	''' Creates instance and connects to meter '''
+    ''' Creates instance and connects to meter '''
     meter = Meter(url, **kwargs)
     meter.connect()
     return meter
 
 def read(url, *args, **kwargs):
-	''' Reads data from list of addresses '''
+    ''' Reads data from list of addresses '''
     with connect(url, **kwargs) as n:
         return [n.readaddr(x, **kwargs) for x in args]
